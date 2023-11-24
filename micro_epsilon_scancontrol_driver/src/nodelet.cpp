bff5c76a7596849b3f7ab0a9a96e6d496082a7f4
nithishkumars <nithishkumarnks@gmail.com>
2023-11-24 15:36:38 +0100

    Clang format updated

--- micro_epsilon_scancontrol_driver/src/nodelet.cpp
+++ micro_epsilon_scancontrol_driver/src/nodelet.cpp
@@ -1,30 +1,28 @@
 #include "micro_epsilon_scancontrol_driver/nodelet.h"
 
-// Register this plugin with pluginlib.  
+// Register this plugin with pluginlib.
 PLUGINLIB_EXPORT_CLASS(scancontrol_driver::DriverNodelet, nodelet::Nodelet)
 
 namespace scancontrol_driver
 {
-    
-    void DriverNodelet::onInit()
-    {
-        // Start scanCONTROL driver
-        driver_.reset(new ScanControlDriver(getNodeHandle(), getPrivateNodeHandle()));
+void DriverNodelet::onInit()
+{
+  // Start scanCONTROL driver
+  driver_.reset(new ScanControlDriver(getNodeHandle(), getPrivateNodeHandle()));
 
-        // spawn device poll thread
-        // running_ = true;
-        // device_thread_ = boost::shared_ptr<boost::thread> (new boost::thread(boost::bind(&DriverNodelet::devicePoll, this)));
-    }
+  // spawn device poll thread
+  // running_ = true;
+  // device_thread_ = boost::shared_ptr<boost::thread> (new boost::thread(boost::bind(&DriverNodelet::devicePoll,
+  // this)));
+}
 
-    /** @brief Device poll thread main loop. */
-    // void DriverNodelet::devicePoll()
-    // {
-    //     while(ros::ok())
-    //         {
-            
-    //         }
-    //     running_ = false;
-    // }
-} 
+/** @brief Device poll thread main loop. */
+// void DriverNodelet::devicePoll()
+// {
+//     while(ros::ok())
+//         {
 
-
+//         }
+//     running_ = false;
+// }
+}  // namespace scancontrol_driver

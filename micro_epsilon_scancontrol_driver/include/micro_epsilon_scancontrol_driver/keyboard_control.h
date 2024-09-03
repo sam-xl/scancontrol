9f722231ab3d778467982eb9f5f8426e23054d3c
panin-ananwa <pananwatanyoo@tudelft.nl>
2024-09-03 11:00:13 +0000

    Add keyboard.h

---
+++ micro_epsilon_scancontrol_driver/include/micro_epsilon_scancontrol_driver/keyboard_control.h
@@ -0,0 +1,9 @@
+// keyboard_control.h
+#ifndef KEYBOARD_CONTROL_H
+#define KEYBOARD_CONTROL_H
+
+#include "micro_epsilon_scancontrol_driver/driver.h"
+
+void handle_keyboard_input(scancontrol_driver::ScanControlDriver& driver);
+
+#endif // KEYBOARD_CONTROL_H
